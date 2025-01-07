6f63c055d9c1ffcc06d1aa93b67cf29ec090b6f5
Idris Gagan <idris.gagan@gmail.com>
2025-01-07 12:04:27 -0600

    adding new files

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+# Add a new key-value pair
+houses["Hermione"] = "Gryffindor"
+
+# Remove a key-value pair
+del houses["Harry"]
+
+# Print the dictionary
+print(houses)
+
+# Print the value for a key
+print(houses["Hermione"])
---
+++ flights.py
@@ -0,0 +1,15 @@
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+    
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+
+
+
+flight = Flight(3)
